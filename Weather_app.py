8251610b7f72d7509ab58d2411f58a404b243c3b
egrif2004 <83315009+egrif2004@users.noreply.github.com>
2021-05-02 22:31:38 -0500

    Add files via upload

---
+++ Weather_app.py
@@ -0,0 +1,44 @@
+import requests
+#website to get data from
+url = api.openweathermap.org/data/2.5/weather?q={city name}&appid={1d50ba15309a5f5d99360157838cc60e}
+#the api assigned to my profile
+api_key ="1d50ba15309a5f5d99360157838cc60e"
+
+
+total_url = url + city_name + api_key
+
+forecast = request.get
+
+
+def show(forecast)
+
+if forecast
+
+print(forecast)
+
+else:
+	error attempt again
+
+
+	def mainm_menu():
+		while true:
+			options=input("zip, city or exit")#give options
+
+			if options
+
+			try zip = int(input("which zip code?: "))
+
+			forecast =
+
+	elif options
+
+	try:
+		city= input("which city do you choose?: ")
+
+		forecast =
+
+		elif options
+
+		break
+		else:
+			print("not correct try again")
